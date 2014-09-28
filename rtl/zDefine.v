d6c215ecfcd51ab46314d6ca0fe78bdeec3746a3
ZhouChuanRuiSAC <ZhouChuanRuiSAC>
2014-09-28 13:32:01 +0800

    1st commit zDefine.v file only

---
+++ rtl/zDefine.v
@@ -0,0 +1,14 @@
+//
+//File: zDefine.v
+//Device: Hardware Independence
+//Created:  2014/9/26 11:26:37
+//Description: the macro header file
+//Revisions: 
+//2014/9/26 11:27:41: created
+//
+
+`define HIGH 1'b1
+`define LOW 1'b0
+`define ON 1'b1
+`define OFF 1'b0
+
